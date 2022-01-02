de122251306152b13265173acf6e1b4f9bf026e7
monishajayasankar <monijaya33@gmail.com>
2022-01-02 21:04:36 +0530

    modified

--- javaprograms/hours.java
+++ javaprograms/hours.java
@@ -1,19 +1,26 @@
-package calendar;
+
+//package calendar;
 import java.util.Calendar;
-public class hours{
+
+public class hours {
     public static void main(String[] args) {
-        Calendar cal=Calendar.getInstance();
-        int hours=cal.get(Calendar.HOUR);
-        System.out.println("hours= "+cal.get(Calendar.HOUR));{
-            if( hours<=12){
-                System.out.println("good morning");}
-            else if(hours>12 && hours<=16){
-                System.out.println("good afternoon");}
-            else if(hours>16 && hours<=21){
-                  System.out.println("good evening");}
-            else{
-              System.out.println("good night");
-            }    
-    }    
-}
+        Calendar cal = Calendar.getInstance();
+        System.out.print(cal.get(Calendar.HOUR_OF_DAY)); // gets hour in 24h format
+        cal.get(Calendar.HOUR); // gets hour in 12h format
+        cal.get(Calendar.MONTH);
+        int hours = cal.get(Calendar.HOUR_OF_DAY);
+
+        System.out.println("hours= " + cal.get(Calendar.HOUR));
+        {
+            if (hours <= 12) {
+                System.out.println("good morning");
+            } else if (hours > 12 && hours <= 14) {
+                System.out.println("good afternoon");
+            } else if (hours > 14 && hours <= 18) {
+                System.out.println("good evening");
+            } else {
+                System.out.println("good night");
+            }
+        }
+    }
 }
