85df4831d85f337737ba44eed70a1828222f23f4
monishajayasankar <monijaya33@gmail.com>
2022-01-04 00:11:31 +0530

    first commit

---
+++ javaprograms/compare.java
@@ -0,0 +1,14 @@
+public class compare{
+   public static void main(String[] args) {
+      String str = "moni";
+      StringBuffer newStr =new StringBuffer();
+      for(int i = str.length()-1; i >= 0 ; i--) {
+         newStr = newStr.append(str.charAt(i));
+      }
+      if(str.equalsIgnoreCase(newStr.toString())) {
+         System.out.println("String is palindrome");
+      } else {
+         System.out.println("String is not palindrome");
+      }
+   }
+}
