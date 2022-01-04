93f99837538f13b1a10b5526437dbfd1e4680666
monishajayasankar <monijaya33@gmail.com>
2022-01-04 21:19:28 +0530

    first commit

---
+++ javaprograms/Alphabet.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Alphabet{
+public static void main(String[] args){
+Scanner in=new Scanner(System.in);
+char c;
+for(c='A';c<='Z';++c)
+System.out.println(c + " ");
+
+}
+}
