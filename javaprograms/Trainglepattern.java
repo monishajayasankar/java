76d32adc73f51af52fcc02c98de18ef7ffcf56c6
monishajayasankar <monijaya33@gmail.com>
2022-01-06 20:56:28 +0530

    pyramidstarpriting

---
+++ javaprograms/Trainglepattern.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Trainglepattern {
+    public static void main(String args[]) {
+      Scanner in=new Scanner(System.in);
+      int i,j,row=5;
+      for(i=0;i<row;i++){
+          for(j=0;j<=i;j++){
+              System.out.println("*");
+          }
+          System.out.println();
+      }
+      
+      }
+      
+}
