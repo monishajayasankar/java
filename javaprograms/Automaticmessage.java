0cd446ed54a757e1f0702d235b51f98cc5d4517f
monishajayasankar <monijaya33@gmail.com>
2022-01-10 20:31:58 +0530

    firstcommit

---
+++ javaprograms/Automaticmessage.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Automaticmessage {
+    public static void main(String args[]) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("me:what is your name: ");
+        System.out.println("you: ");
+        String name=in.next();
+        System.out.println("me:where are you from : ");
+        System.out.println("you: ");
+        String place=in.next();
+        System.out.println("enter your fav food: ");
+        System.out.println("me: ");
+        String food=in.next();
+        System.out.println("hi"+name+".");
+        System.out.println("from"+place+" .");
+        System.out.println("yummy"+food+":)");
+       
+        
+        
+      
+    }
+}
